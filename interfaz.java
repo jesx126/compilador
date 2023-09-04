ee2bbd7d41eaf59bf727ccd2e71036a92f166a45
jesx <jeresergor@gmail.com>
2023-09-04 16:58:50 -0600

    implementación de JFrame con su Jpanel,método donde se agregaran los componentes de la interfaz y la creación del objeto para llamar la interfaz en el archivo main

--- interfaz.java
+++ interfaz.java
@@ -1,3 +1,25 @@
-public class interfaz {
+import java.awt.Color;
+
+import javax.swing.*;
+public class interfaz extends JFrame{
+    JPanel p= new JPanel();
     
+   public void initcomp(){
+   interfaz miAplicacion = new interfaz();
+       miAplicacion.pack();
+       miAplicacion.setVisible(true);
+       miAplicacion.setSize(500, 500);
+       miAplicacion.setLocationRelativeTo(null);
+       miAplicacion.setDefaultCloseOperation(miAplicacion.EXIT_ON_CLOSE);
+        miAplicacion.getContentPane().add(p);
+        comp();
+        }
+        public void comp(){
+            p.setBackground(Color.BLACK);
+            
+        }
+        
+    
+
+
 }
--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 public class main{
     public static void main(String[] args) {
-        System.out.println("Prueba de push");
+
+        interfaz in = new interfaz();
+        in.initcomp();
+        
     }
 }
